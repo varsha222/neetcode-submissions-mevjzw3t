8abf7f0b1b41587b10e5e7a205ec8f360c1c6b16
varsha222 <33314705+varsha222@users.noreply.github.com>
2026-03-30 13:30:12 +0530

    Add: score-of-a-string - submission-4

---
+++ Data Structures & Algorithms/score-of-a-string/submission-4.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int ScoreOfString(string s) {
+        int sum=0;
+        for(int i=0;i<s.Length;i++)
+        {
+            if(i+1<s.Length)
+            {
+                sum=sum+Math.Abs(s[i+1]-s[i]);
+            }
+        }
+        return sum;
+    }
+}
