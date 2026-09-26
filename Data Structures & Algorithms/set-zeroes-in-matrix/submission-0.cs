6c382517775925f722784f4dddd828abcb435fa4
varsha222 <33314705+varsha222@users.noreply.github.com>
2026-09-26 15:55:07 +0530

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public void SetZeroes(int[][] matrix) {
+        int m=matrix.Length;
+        int n=matrix[0].Length;
+        HashSet<int> rows= new HashSet<int>();
+        HashSet<int> cols = new HashSet<int>();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    rows.Add(i);
+                    cols.Add(j);
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(rows.Contains(i) || cols.Contains(j))
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        
+    }
+}
