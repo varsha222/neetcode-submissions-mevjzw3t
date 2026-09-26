453171ae6d155d5c91516abd0f41fa7a6f4f37f5
varsha222 <33314705+varsha222@users.noreply.github.com>
2026-09-26 14:30:10 +0530

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        int n = matrix.GetLength(0);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            int left=0;
+            int right=n-1;
+            while(left<right)
+            {
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+
+            }
+        }
+    }
+}
